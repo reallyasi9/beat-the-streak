bf2cad0b860a4403194b9917689a2c699d0db0c8
Phillip Killewald <phillip.killewald@gmail.com>
2015-11-29 01:38:41 -0500

    All my work is gone.

--- bts-types.go
+++ bts-types.go
@@ -25,32 +25,75 @@ func (s *selection) Set(value string) error {
 	return nil
 }
 
+func (s *selection) CopyWithoutTeam(t string) (selection, error) {
+	out := make(selection, len(*s))
+	copy(out, *s)
+	found := -1
+	for i, sel := range out {
+		if sel == t {
+			found = i
+			out = append(out[:i], out[i+1:]...)
+			break
+		}
+	}
+	if found < 0 {
+		return out, fmt.Errorf("team %s not found", t)
+	}
+	return out, nil
+}
+
 type probabilityMap map[string][]float64
 
-func (p probabilityMap) FilterTeams(s selection) (probabilityMap, error) {
+func (p *probabilityMap) KeepTeams(s selection) error {
 	out := make(probabilityMap)
 	for _, sel := range s {
-		v, exists := p[sel]
-		if exists == false {
-			return out, fmt.Errorf("key %s not present in map", sel)
+		v, exists := (*p)[sel]
+		if !exists {
+			return fmt.Errorf("key %s not present in map", sel)
 		}
 		out[sel] = v
 	}
-	return out, nil
+	*p = out
+	return nil
 }
 
-func (p probabilityMap) FilterWeeks(minWeek int) (probabilityMap, error) {
+func (p *probabilityMap) CopyWithoutTeam(t string) (probabilityMap, error) {
 	out := make(probabilityMap)
-	for k, v := range p {
-		out[k] = v[minWeek-1:]
+	_, exists := (*p)[t]
+	if !exists {
+		return out, fmt.Errorf("key %s not present in map", t)
+	}
+	for k, v := range *p {
+		if k == t {
+			continue
+		}
+		out[k] = v
 	}
 	return out, nil
 }
 
+func (p *probabilityMap) FilterWeeks(minWeek int) error {
+	for k, v := range *p {
+		(*p)[k] = v[minWeek-1:]
+	}
+	return nil
+}
+
+func (p *probabilityMap) TotalProb(s selection) (float64, error) {
+	prob := float64(1)
+	for i, sel := range s {
+		if len((*p)[sel]) != len(s) {
+			return 0., fmt.Errorf("length of selection (%d) does not match remaining weeks (%d)", len(s), len((*p)[sel]))
+		}
+		prob *= (*p)[sel][i]
+	}
+	return prob, nil
+}
+
 // Convenience type, so I can return both a probability and a selection from a goroutine
-type permutation struct {
+type orderperm struct {
 	prob   float64
 	perm   selection
-	ddprob float64
+	ddteam string
 	ddweek int
 }
