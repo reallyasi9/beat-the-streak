98cbf79c87b8e1f3e315417e35179329a6d16bcb
Phil Killewald <phillip.killewald@gmail.com>
2017-11-27 21:32:10 -0500

    Fixing paths to be faster and use threads better.

--- paths/bts-paths.go
+++ paths/bts-paths.go
@@ -3,107 +3,50 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 
-	"runtime"
-
 	"../bts"
-	yaml "gopkg.in/yaml.v2"
+
+	"runtime"
 )
 
 var numCPU = runtime.GOMAXPROCS(0)
 var outcomesFile = flag.String("outcomes",
 	"outcomes.yaml",
 	"YAML `file` containing outcomes for B1G schedule (1 = win, 0 = loss)")
-var scheduleFile = flag.String("schedule",
-	"schedule.yaml",
-	"YAML `file` containing B1G schedule")
 
 func main() {
 	flag.Parse()
 
-	schedule, err := bts.MakeSchedule(*scheduleFile)
+	oc, err := makeOutcomes(*outcomesFile)
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("Made schedule %v", schedule)
-
-	probs, err := makeOutcomes(*outcomesFile, schedule)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Constructed outcomes %v", probs)
-
-	// The only "Player" in this context is a single player with all picks remaining
-	player := make(bts.Player, len(schedule))
-	i := 0
-	for tn := range schedule {
-		player[i] = string(tn)
-		i++
-	}
-
-	log.Printf("Made player %v", player)
+	log.Printf("Constructed outcomes %v", oc)
 
 	// Here we go.
-	// Loop through the (one) unique users
-	results := make(chan bts.StreaksByProb)
-	jobs := make(chan bts.Player)
-	go worker(jobs, results, *probs)
-	jobs <- player
-	close(jobs)
+	results := worker(oc)
 
 	// Drain the results now
-	result := <-results
-	nSolutions := 0
-	for _, res := range result {
-		if res.Streak == nil || res.Prob == 0 {
-			continue
-		}
+	var nSolutions uint64
+	for res := range results {
+
 		nSolutions++
-		for _, tn := range res.Streak.Teams {
+		for _, tn := range res.Teams {
 			fmt.Printf("%s,", tn)
 		}
-		if res.Streak.DD != nil {
-			fmt.Printf("%d,%s", res.Streak.DD.Week, res.Streak.DD.Team)
+		if res.DD != nil {
+			fmt.Printf("%d,%s", res.DD.Week, res.DD.Team)
 		} else {
 			fmt.Print("0,")
 		}
 		fmt.Println()
 	}
 
-	fmt.Printf("Paths found: %d\n", nSolutions)
-	if nSolutions == 100000000 {
-		fmt.Println("Number of paths reached maximum: more paths might exist!")
-	}
-
-	close(results)
-
 }
 
-func worker(jobs <-chan bts.Player, results chan<- bts.StreaksByProb, probs bts.Probabilities) {
-	for p := range jobs {
-		results <- p.BestStreaks(probs, true, 100000000)
-	}
-}
-
-func makeOutcomes(fn string, sched bts.Schedule) (*bts.Probabilities, error) {
-	outcomeYaml, err := ioutil.ReadFile(fn)
-	if err != nil {
-		return nil, err
-	}
-
-	p := make(bts.Probabilities)
-	err = yaml.Unmarshal(outcomeYaml, p)
-	if err != nil {
-		return nil, err
-	}
-
-	for k, v := range p {
-		if len(v) != 13 {
-			return nil, fmt.Errorf("outcomes for team %s incorrect: expected %d, got %d", k, 13, len(v))
-		}
-	}
-
-	return &p, nil
+func worker(oc *outcomes) chan bts.Streak {
+	out := make(chan bts.Streak)
+	go oc.countPaths(out)
+	return out
 }
---
+++ paths/outcomes.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sync"
+
+	"../bts"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type outcomes map[string][]bool
+
+func makeOutcomes(fn string) (*outcomes, error) {
+	outcomeYaml, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return nil, err
+	}
+
+	o := make(outcomes)
+	err = yaml.Unmarshal(outcomeYaml, o)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range o {
+		if len(v) != 13 {
+			return nil, fmt.Errorf("outcomes for team %s incorrect: expected 13, got %d", k, len(v))
+		}
+	}
+
+	return &o, nil
+}
+
+func (oc *outcomes) countPaths(out chan<- bts.Streak) {
+	defer close(out)
+
+	jobs := make(chan bts.TeamList, 100)
+	results := make(chan bts.TeamList, 100)
+	wg := new(sync.WaitGroup)
+	for i := 0; i < numCPU; i++ {
+		wg.Add(1)
+		go cpWorker(jobs, results, oc, wg)
+	}
+
+	wg2 := new(sync.WaitGroup)
+	for i := 0; i < numCPU; i++ {
+		wg2.Add(1)
+		go streakWorker(results, out, oc, wg)
+	}
+
+	for ddt := range *oc {
+		tl := make(bts.TeamList, 0)
+		for t := range *oc {
+			if t == ddt {
+				continue
+			}
+			tl = append(tl, bts.Team(t))
+		}
+
+		jobs <- tl
+	}
+
+	wg.Wait()
+
+	close(results)
+
+	wg2.Wait()
+}
+
+func (oc *outcomes) validate(tl bts.TeamList) bool {
+	for week, team := range tl {
+		if !(*oc)[string(team)][week] {
+			return false
+		}
+	}
+	return true
+}
+
+func cpWorker(jobs chan bts.TeamList, results chan<- bts.TeamList, oc *outcomes, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for j := range jobs {
+		c := make(chan bts.TeamList, 100)
+		go bts.TeamPermute(j, c)
+		for tl := range c {
+			if oc.validate(tl) {
+				results <- tl
+			}
+		}
+	}
+}
+
+func streakWorker(jobs chan bts.TeamList, results chan<- bts.Streak, oc *outcomes, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for tl := range jobs {
+		found := make(map[string]bool)
+		for _, team := range tl {
+			found[string(team)] = true
+		}
+
+		for ddt, v := range *oc {
+			if _, ok := found[ddt]; ok {
+				continue
+			}
+
+			for week, wl := range v {
+				if !wl {
+					continue
+				}
+				dd := &bts.DoubleDown{Team: bts.Team(ddt), Week: week}
+				s := bts.Streak{Teams: tl, DD: dd}
+				results <- s
+			}
+		}
+	}
+}
