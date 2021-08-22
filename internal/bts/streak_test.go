c0953f4362dfd4227e556f653802485f787ffbff
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-08-22 11:32:05 -0400

    Make tests work again

--- internal/bts/streak_test.go
+++ internal/bts/streak_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestNewStreak(t *testing.T) {
-	rem := Remaining{"A", "B", "C", "D"}
+	rem := Remaining{Team{"A"}, Team{"B"}, Team{"C"}, Team{"D"}}
 	ppw := []int{0, 2, 0, 1, 1}
 	itr := []int{1, 2, 3, 0}
 	s := NewStreak(rem, ppw, itr)
